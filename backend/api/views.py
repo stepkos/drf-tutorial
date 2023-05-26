b4b3fef714e6cf2adb5c85fed564a72b52fec8b6
stepkos <stepkos@wp.pl>
2023-05-26 22:05:32 +0200

    First step in DRF

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,16 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,4 +3,6 @@ import requests
 endpoint = "http://127.0.0.1:8000/api"
 
 get_response = requests.get(endpoint, json={"product_id": 123})
+# print(get_response.headers)
+# print(get_response.text)
 print(get_response.json())
