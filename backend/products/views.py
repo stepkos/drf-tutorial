7c192105cffb2466f4fa1ae8933308545362cb6c
stepkos <stepkos@wp.pl>
2023-05-27 09:20:53 +0200

    Mixins

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # from django.hhtp import Http404
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+# Kazde takie APIView sklada sie z mixinow (zobacz implementacje)
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -58,6 +59,39 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
 #     # lookup_field = 'pk'
 
 #     # Product.objects.get(pk=1)
+
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin, 
+    mixins.RetrieveModelMixin, 
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer = ProductSerializer()
+    lookup_field = 'pk'
+    
+    # http get
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    # http post
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content = 'this is a single view doing stuff'
+        serializer.save(content=content)
+
+
+    
 
 
 @api_view(['GET', 'POST'])
